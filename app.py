3bde8b46eae1a1e9723dd5bf0cd98e4e841499b7
pchatman <34121076+pchatman@users.noreply.github.com>
2025-09-29 16:36:23 -0400

    style: "Remove test code:

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ def index():
         {'city': 'Monrovia', 'timezone': 'Africa/Monrovia'},
         {'city': 'Syndney', 'timezone': 'Australia/Sydney'}
     ]
-
-    # return render_template('index.html', timezones=world_timezones)
+    
     return render_template('index.html', world_timezones=world_timezones)
 
 if __name__ == '__main__':
